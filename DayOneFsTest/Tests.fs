b8405fa3b9fb0c63ba669fc9251ed615b5f22476
Sean Sedlock <sean.sedlock@gmail.com>
2021-12-04 16:09:28 -0600

    Adding additional test methods

--- DayOneFsTest/Tests.fs
+++ DayOneFsTest/Tests.fs
@@ -12,7 +12,29 @@ type TestClass () =
         Assert.IsTrue(true);
 
     [<TestMethod>]
+    member this.ComparerCountsExpectedIncreasesFromTestInput () =
+        let input = [ 199;
+            200;
+            208;
+            210;
+            200;
+            207;
+            240;
+            269;
+            260;
+            263]
+        let expected = 7
+        let actual = Comparer.CountIncreasesInSequence input
+        Assert.AreEqual(expected, actual)
+
+    [<TestMethod>]
     member this.ComparerCountsExpectedIncreasesWhenAllIncreasing () =
         let expected = 2
         let actual = Comparer.CountIncreasesInSequence [1;2;3]
+        Assert.AreEqual(expected, actual)
+
+    [<TestMethod>]
+    member this.ComparerReturnsZeroIfArrayHasOnlyOneElement () =
+        let expected = 0
+        let actual = Comparer.CountIncreasesInSequence []
         Assert.AreEqual(expected, actual)
